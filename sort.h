03f62c0501c31b1bc1211821874cba8bb4dcc866
abdelrahman atef morsy <abdelrahman.atef.morsy@gmail.com>
2024-02-16 07:17:23 -0800

    edit 1-insertion

--- sort.h
+++ sort.h
@@ -1,5 +1,8 @@
 #ifndef SORT_H
 #define SORT_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -7,16 +10,13 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <stddef.h>
 typedef struct listint_s
 {
 	const int n;
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
-listint_t* swapnode(listint_t* node, listint_t** list);
+listint_t *swapnode(listint_t *node, listint_t **list);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void insertion_sort_list(listint_t **list);
