1400ddd5ef0b6c6a2b8bea65daf662ac0a0d74bb
Saged2003 <sagedryan775@gmail.com>
2024-02-17 09:01:33 -0800

    saged

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,10 +5,11 @@
  * using the bubble sort algorithm
  * @array: the array to be sorted
  * @size: the size of the array
+ *
  */
 void bubble_sort(int *array, size_t size)
 {
-	int t;
+	int temp;
 	size_t i, j;
 
 	if (array == NULL || size < 2)
@@ -17,13 +18,13 @@ void bubble_sort(int *array, size_t size)
 	}
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - 1 - i; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				t = array[j];
+				temp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = t;
+				array[j + 1] = temp;
 				print_array(array, size);
 			}
 		}
