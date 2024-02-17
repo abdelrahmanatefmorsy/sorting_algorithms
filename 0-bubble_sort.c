3ce4532c856325ac567865e4b0c90e93c0261be6
Saged2003 <sagedryan775@gmail.com>
2024-02-17 09:11:30 -0800

    saged

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,12 +14,12 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 
 	if (array == NULL || size < 2)
+	{
 		return;
-	i = 0;
-
+	}
 	while (i < size)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size -1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
