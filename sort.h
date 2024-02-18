7f68d10202a347d1bf72a247e876dee906fd2604
Saged2003 <sagedryan775@gmail.com>
2024-02-18 10:57:48 -0800

    saged

--- sort.h
+++ sort.h
@@ -20,5 +20,6 @@ listint_t *swapnode(listint_t *node, listint_t **list);
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 #endif
